70f2988081a2b3dbea105f9c0c6a4f83fc0386ea
kpmn819 <kpneff@comcast.net>
2023-07-25 09:39:49 -0400

    after new laptop

--- generic_game.py
+++ generic_game.py
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # attempt to do a generic game format with classes
-
+# 
 # IMPORTS START --------------------------------------------------
 # makes extensive use of pygame to blit the screen
 
